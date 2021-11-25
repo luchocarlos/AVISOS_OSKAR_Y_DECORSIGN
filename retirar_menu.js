e41ba9bdb1a3197cc876f24d894d03091bd0afb3
luchocarlos <luchopulimar@gmail.com>
2021-11-24 22:58:06 -0600

    version-1.1

--- retirar_menu.js
+++ retirar_menu.js
@@ -1,11 +1,26 @@
 const menu = document.getElementById('menu');
 const toggle = document.getElementById('toggle');
 const opcion = document.getElementById('opcion')
+const opcion1 = document.getElementById('opcion1')
+const opcion2 = document.getElementById('opcion2')
+const opcion3 = document.getElementById('opcion3')
 
 menu.addEventListener("click", function(){
     toggle.classList.toggle("show")
 })
 
 opcion.addEventListener("click", function(){
+        toggle.classList.remove("show")
+})
+
+opcion1.addEventListener("click", function(){
+        toggle.classList.remove("show")
+})
+
+opcion2.addEventListener("click", function(){
+        toggle.classList.remove("show")
+})
+
+opcion3.addEventListener("click", function(){
         toggle.classList.remove("show")
 })
